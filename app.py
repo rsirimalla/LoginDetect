791d388e897a58c171a2793a649bb40c709f5176
rsirimalla <ramesh.sirimalla@gmail.com>
2018-12-28 18:41:17 -0500

    Refactor

--- app.py
+++ app.py
@@ -123,7 +123,7 @@ def login():
                                                       SPEED_THRESHOLD) if 'speed' in response['subsequentIpAccess'] else 'NA'
         return jsonify(response), 200
     except CustomException as e:
-        return jsonify(e.to_dict()), 201
+        return jsonify(e.to_dict()), e.status_code
     except Exception as e:
         return jsonify({'message': 'Internal server Error'}), 500
 
--- tests/test_app.py
+++ tests/test_app.py
@@ -7,7 +7,7 @@ from exception import CustomException
 import geoip
 import validation
 import json
-import test_data
+import testdata
 
 
 class LoginTests(unittest.TestCase):
@@ -21,12 +21,12 @@ class LoginTests(unittest.TestCase):
         pass
 
     def test_no_username(self):
-        result = self.app.post('/v1', data=json.dumps(test_data.request_no_username), content_type='application/json')
+        result = self.app.post('/v1', data=json.dumps(testdata.request_no_username), content_type='application/json')
         self.assertEqual(result.status_code, 400)
         self.assertEqual(result.data, '{"message":"\'username\' is a required property"}\n')
 
     def test_invalid_ip(self):
-        result = self.app.post('/v1', data=json.dumps(test_data.request_invalid_ip), content_type='application/json')
+        result = self.app.post('/v1', data=json.dumps(testdata.request_invalid_ip), content_type='application/json')
         self.assertEqual(result.status_code, 400)
         self.assertEqual(result.data, '{"message":"Invalid IP or Unable to get geo location for 20600.81.252.6"}\n')
 
@@ -35,16 +35,15 @@ class LoginTests(unittest.TestCase):
     @mock.patch('app.insert_db')
     def test_geo_travel_flags(self, mock_insert_db, mock_location, mock_access_details):
         mock_insert_db.return_value = True
-        mock_location.return_value = test_data.mock_location1
+        mock_location.return_value = testdata.mock_location1
         mock_access_details.return_value = {}
 
-        result = self.app.post('/v1', data=json.dumps(test_data.request_good), content_type='application/json')
+        result = self.app.post('/v1', data=json.dumps(testdata.request_good), content_type='application/json')
         response = json.loads(result.data)
         self.assertEqual(result.status_code, 200)
         self.assertEqual(response['currentGeo']['radius'], 200)
         self.assertEqual(response['travelToCurrentGeoSuspicious'], 'NA')
         self.assertEqual(response['travelFromCurrentGeoSuspicious'], 'NA')
-
 
     @mock.patch('app.geoip.haversine')
     @mock.patch('app.geoip.get_location')
@@ -52,29 +51,28 @@ class LoginTests(unittest.TestCase):
     @mock.patch('app.insert_db')
     def test_speed_calculation_suspicious(self, mock_insert_db, mock_access_details_from_db, mock_geoip_get_location, mock_haversine):
         mock_insert_db.return_value = True
-        mock_access_details_from_db.return_value = test_data.mock_speed_db_result
-        mock_geoip_get_location.return_value = test_data.mock_location1
+        mock_access_details_from_db.return_value = testdata.mock_speed_db_result
+        mock_geoip_get_location.return_value = testdata.mock_location1
         mock_haversine.return_value = 500
 
-        result = self.app.post('/v1', data=json.dumps(test_data.mock_request_speed_calc), content_type='application/json')
+        result = self.app.post('/v1', data=json.dumps(testdata.mock_request_speed_calc), content_type='application/json')
         response = json.loads(result.data)
         self.assertEqual(result.status_code, 200)
         self.assertEqual(response['subsequentIpAccess']['speed'], 900)
         self.assertEqual(response['travelFromCurrentGeoSuspicious'], True)
-
 
     @mock.patch('app.geoip.haversine')
     @mock.patch('app.geoip.get_location')
     @mock.patch('app.get_access_details_from_db')
     @mock.patch('app.insert_db')
     def test_speed_calculation_not_suspicious(self, mock_insert_db, mock_access_details_from_db, mock_geoip_get_location, mock_haversine):
-            mock_insert_db.return_value = True
-            mock_access_details_from_db.return_value = test_data.mock_speed_db_result1
-            mock_geoip_get_location.return_value = test_data.mock_location1
-            mock_haversine.return_value = 500
+        mock_insert_db.return_value = True
+        mock_access_details_from_db.return_value = testdata.mock_speed_db_result1
+        mock_geoip_get_location.return_value = testdata.mock_location1
+        mock_haversine.return_value = 500
 
-            result = self.app.post('/v1', data=json.dumps(test_data.mock_request_speed_calc1), content_type='application/json')
-            response = json.loads(result.data)
-            self.assertEqual(result.status_code, 200)
-            self.assertEqual(response['subsequentIpAccess']['speed'], 90)
-            self.assertEqual(response['travelFromCurrentGeoSuspicious'], False)
+        result = self.app.post('/v1', data=json.dumps(testdata.mock_request_speed_calc1), content_type='application/json')
+        response = json.loads(result.data)
+        self.assertEqual(result.status_code, 200)
+        self.assertEqual(response['subsequentIpAccess']['speed'], 90)
+        self.assertEqual(response['travelFromCurrentGeoSuspicious'], False)
