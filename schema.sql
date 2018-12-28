fead91f9b00f165a3d861034b22bed09597cbea3
rsirimalla <ramesh.sirimalla@gmail.com>
2018-12-28 14:16:50 -0500

    Uniq on event uuid

--- schema.sql
+++ schema.sql
@@ -4,7 +4,7 @@ DROP TABLE IF EXISTS login_geo_location;
 CREATE TABLE login_geo_location (
     id integer PRIMARY KEY,
     username text NOT NULL,
-    event_uuid text NOT NULL,
+    event_uuid text NOT NULL UNIQUE,
     ip_address text NOT NULL,
     unix_timestamp timestamp NOT NULL,
     lat real NOT NULL,
